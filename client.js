6fb84776bb7f3a8765905d588ecb3bdde72ebf71
Francis Pellerin-Venne <francispellerinvenne@hotmail.fr>
2020-07-01 17:17:03 +0000

    adding some movement to the snake

--- client.js
+++ client.js
@@ -13,14 +13,20 @@ const connect = function() {
 
   conn.on('connect', () => {
     console.log('Hello player, you are now in my game. Good luck and have fun!');
-  }); 
-  
-  conn.on('connect', () => {
+
     conn.write('Name: FPV');
+   /*  conn.write('Move: down');
+    conn.write('Move: down');
+    conn.write('Move: left');
+    conn.write('Move: left'); 
+    setInterval(() => {
+      conn.write('Move: up');
+    }, 1000) */
   });
 
+
+
   conn.on('data', (data) => {
-    /* console.log('You ded cause you idled'); */
     console.log(data);
   });
   
